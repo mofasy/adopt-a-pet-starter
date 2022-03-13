6e2f503cbf6f8ddfae7a3c502f67987653d383ef
mofasy <sayed.mfahad@gmail.com>
2022-03-13 17:08:44 +0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React from 'react';
 import HomePage from './pages/home';
 import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
       <HomePage />
-    </div>
+    </Router>
   );
 }
 
